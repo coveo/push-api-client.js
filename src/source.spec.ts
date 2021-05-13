5102b90e740f1764aa4fb965a5a47cabeb600498
Olivier Lamothe <olamothe@coveo.com>
2021-05-13 09:35:23 -0400

    test(source): improve test coverage for source (#18)
    https://coveord.atlassian.net/browse/CDX-316

--- src/source.spec.ts
+++ src/source.spec.ts
@@ -1,10 +1,12 @@
 /* eslint-disable node/no-unpublished-import */
 jest.mock('@coveord/platform-client');
-import mockAxios from 'jest-mock-axios';
+jest.mock('axios');
 import PlatformClient, {SourceVisibility} from '@coveord/platform-client';
-import {Source} from './source';
+import {BatchUpdateDocuments, Source} from './source';
 import {mocked} from 'ts-jest/utils';
 import {DocumentBuilder} from './documentBuilder';
+import axios from 'axios';
+const mockAxios = axios as jest.Mocked<typeof axios>;
 
 const mockedPlatformClient = mocked(PlatformClient);
 const mockCreate = jest.fn();
@@ -18,11 +20,21 @@ mockedPlatformClient.mockImplementation(
 );
 
 describe('Source', () => {
+  let source: Source;
+  beforeEach(() => {
+    source = new Source('the_key', 'the_org');
+  });
+
+  const expectedDocumentsHeaders = {
+    headers: {
+      Accept: 'application/json',
+      Authorization: 'Bearer the_key',
+      'Content-Type': 'application/json',
+    },
+  };
+
   it('should call platform client on creation', () => {
-    new Source('the_key', 'the_org').create(
-      'the_name',
-      SourceVisibility.SHARED
-    );
+    source.create('the_name', SourceVisibility.SHARED);
 
     expect(mockCreate).toHaveBeenCalledWith({
       name: 'the_name',
@@ -33,7 +45,7 @@ describe('Source', () => {
   });
 
   it('should call axios on add document', () => {
-    new Source('the_key', 'the_org').addOrUpdateDocument(
+    source.addOrUpdateDocument(
       'the_id',
       new DocumentBuilder('the_uri', 'the_title')
     );
@@ -41,13 +53,102 @@ describe('Source', () => {
     expect(mockAxios.put).toHaveBeenCalledWith(
       'https://api.cloud.coveo.com/push/v1/organizations/the_org/sources/the_id/documents?documentId=the_uri',
       expect.objectContaining({title: 'the_title'}),
-      {
-        headers: {
-          Accept: 'application/json',
-          Authorization: 'Bearer the_key',
-          'Content-Type': 'application/json',
-        },
-      }
+      expectedDocumentsHeaders
     );
+  });
+
+  describe('calls axios when doing delete olderthan', () => {
+    const expectCorrectOrderingId = (id: number | string) => {
+      expect(mockAxios.delete).toHaveBeenCalledWith(
+        `https://api.cloud.coveo.com/push/v1/organizations/the_org/sources/the_id/documents/olderthan?orderingId=${id}`,
+        expectedDocumentsHeaders
+      );
+    };
+
+    it('with a date', () => {
+      const now = new Date();
+      source.deleteDocumentsOlderThan('the_id', now);
+      expectCorrectOrderingId(now.valueOf());
+    });
+
+    it('with a date string', () => {
+      source.deleteDocumentsOlderThan('the_id', '2001/01/01');
+      expectCorrectOrderingId(new Date('2001/01/01').valueOf());
+    });
+
+    it('with a timestamp', () => {
+      const nowInTimestamp = new Date().valueOf();
+      source.deleteDocumentsOlderThan('the_id', nowInTimestamp);
+      expectCorrectOrderingId(nowInTimestamp);
+    });
+  });
+
+  it('should call axios on delete', () => {
+    source.deleteDocument('the_id', 'the_uri', true);
+    expect(mockAxios.delete).toHaveBeenCalledWith(
+      'https://api.cloud.coveo.com/push/v1/organizations/the_org/sources/the_id/documents?documentId=the_uri&deleteChildren=true',
+      expectedDocumentsHeaders
+    );
+  });
+
+  describe('when doing batch update', () => {
+    let batch: BatchUpdateDocuments;
+    beforeEach(() => {
+      batch = {
+        addOrUpdate: [
+          new DocumentBuilder('the_uri_1', 'the_title_1'),
+          new DocumentBuilder('the_uri_2', 'the_title_2'),
+        ],
+        delete: [{documentId: 'the_uri_3', deleteChildren: true}],
+      };
+
+      mockAxios.post.mockImplementationOnce((url: string) => {
+        if (url.match(/files/)) {
+          return Promise.resolve({
+            data: {
+              uploadUri: 'https://fake.upload.url',
+              fileId: 'file_id',
+              requiredHeaders: {foo: 'bar'},
+            },
+          });
+        }
+        return Promise.resolve();
+      });
+    });
+
+    it('should create a file container', async () => {
+      await source.batchUpdateDocuments('the_id', batch);
+      expect(mockAxios.post).toHaveBeenCalledWith(
+        'https://api.cloud.coveo.com/push/v1/organizations/the_org/files',
+        expect.objectContaining({}),
+        expectedDocumentsHeaders
+      );
+    });
+
+    it('should upload files to container with returned upload uri and required headers ', async () => {
+      await source.batchUpdateDocuments('the_id', batch);
+      expect(mockAxios.put).toHaveBeenCalledWith(
+        'https://fake.upload.url/',
+        expect.objectContaining({
+          addOrUpdate: expect.arrayContaining([
+            expect.objectContaining({documentId: 'the_uri_1'}),
+            expect.objectContaining({documentId: 'the_uri_2'}),
+          ]),
+          delete: expect.arrayContaining([
+            expect.objectContaining({documentId: 'the_uri_3'}),
+          ]),
+        }),
+        {headers: {foo: 'bar'}}
+      );
+    });
+
+    it('should push content to source with returned fileId', async () => {
+      await source.batchUpdateDocuments('the_id', batch);
+      expect(mockAxios.put).toHaveBeenCalledWith(
+        'https://api.cloud.coveo.com/push/v1/organizations/the_org/sources/the_id/documents/batch?fileId=file_id',
+        {},
+        expectedDocumentsHeaders
+      );
+    });
   });
 });
