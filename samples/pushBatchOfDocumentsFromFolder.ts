cd7231e33b16f676a96d1b99ec63796b36f8fddd
y-lakhdar <ylakhdar@coveo.com>
2022-01-13 16:20:48 -0500

    Add sample

---
+++ samples/pushBatchOfDocumentsFromFolder.ts
@@ -0,0 +1,55 @@
+import {
+  Source,
+  UploadBatchCallback,
+  UploadBatchCallbackData,
+} from '@coveo/push-api-client';
+
+function onSuccessCallback({batch, files, res}: UploadBatchCallbackData) {
+  const numAdded = batch.length;
+  let fileNames = files.slice(0, 5).join(', ');
+  if (files.length > 5) {
+    fileNames += ` and ${files.length - 5} more ...`;
+  }
+  console.log(
+    `Success: ${numAdded} document${
+      numAdded > 1 ? 's' : ''
+    } accepted by the Push API from ${fileNames}.`
+  );
+  console.log(`         Status code: ${(res?.status, res?.statusText)}
+      `);
+}
+
+function onErrorCallback(err: unknown, {files}: UploadBatchCallbackData) {
+  console.log(`Something went wrong while uploading files ${files}`, err);
+}
+
+async function main() {
+  const source = new Source('my_api_key', 'my_coveo_organization_id');
+  source.setSourceStatus('my_source_id', 'REFRESH');
+
+  /**
+   * You can push from files and folders containing JSON files.
+   * Only JSON files will be considered if the folder contains other filetype extensions.
+   */
+  const entries = [
+    '/path/to/folder',
+    '/path/to/fileA.json',
+    '/path/to/fileB.json',
+  ];
+
+  const callback: UploadBatchCallback = (
+    err: unknown,
+    data: UploadBatchCallbackData
+  ) => {
+    if (err) {
+      onErrorCallback(err, data);
+    } else {
+      onSuccessCallback(data);
+    }
+  };
+
+  await source.batchUpdateDocumentsFromFiles('my_source_id', entries, callback);
+  source.setSourceStatus('my_source_id', 'IDLE');
+}
+
+main();
