0d40d60e943964ac79ccf139d544fff024736409
y-lakhdar <ylakhdar@coveo.com>
2022-01-13 10:02:16 -0500

    fix UTs

--- src/validation/parseFile.spec.ts
+++ src/validation/parseFile.spec.ts
@@ -17,31 +17,37 @@ describe('parseFile', () => {
 
   it.each([
     {
+      title: 'should fail on missing title',
       fileName: 'noTitle.json',
       error: 'title: value is required.',
     },
     {
+      title: 'should fail on missing documentID',
       fileName: 'noId.json',
       error: 'documentid: value is required.',
     },
     {
+      title: 'should fail on missing identity',
       fileName: 'noIdentity.json',
       error: 'allowedpermissions:  value does not contain identity',
     },
     {
+      title: 'should fail on identity with an invalid string',
       fileName: 'identityNotAString.json',
       error: 'allowedpermissions:   value is not a string.',
     },
     {
+      title: 'should fail on allow anonymous with an invalid boolean',
       fileName: 'identityAllowAnonymousNotABoolean.json',
       error: 'allowanonymous: value is not a boolean.',
     },
     {
+      title: 'should fail on identityType with an invalid value',
       fileName: 'identityTypeInvalidValue.json',
       error:
         'allowedpermissions:   value should be one of: UNKNOWN, USER, GROUP, VIRTUAL_GROUP.',
     },
-  ])('should fail on missing $missingProp', ({fileName, error}) => {
+  ])('$title', ({fileName, error}) => {
     const file = join(pathToStub, 'jsondocuments', fileName);
     expect(parse(file)).toThrow(
       new InvalidDocument(
