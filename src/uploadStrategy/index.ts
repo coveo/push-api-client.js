d0651cbd0d7ea8cb5197c22ad9bf58c8db9e2922
y-lakhdar <ylakhdar@coveo.com>
2022-03-21 11:51:04 -0400

    add strategy class

---
+++ src/uploadStrategy/index.ts
@@ -0,0 +1,3 @@
+// export * from './fileContainerStrategy'; // TODO: CDX-884
+export * from './strategy';
+// export * from './streamChunkStrategy'; // TODO: CDX-884
---
+++ src/uploadStrategy/strategy.ts
@@ -0,0 +1,29 @@
+import {AxiosResponse} from 'axios';
+import type {
+  FailedUploadCallback,
+  SuccessfulUploadCallback,
+} from '../help/fileConsumer';
+import type {BatchUpdateDocuments, ConcurrentProcessing} from '../interfaces';
+
+export interface UploadStrategy {
+  /**
+   * Upload documents from a list of file path
+   *
+   * @param {string[]} files paths to upload
+   * @param {Required<ConcurrentProcessing>} processingConfig
+   */
+  uploadFiles: (
+    files: string[],
+    processingConfig: Required<ConcurrentProcessing>
+  ) => Promise<{
+    onBatchError: (callback: FailedUploadCallback) => void;
+    onBatchUpload: (callback: SuccessfulUploadCallback) => void;
+    done: () => Promise<void>;
+  }>;
+  /**
+   * Upload a batch of documents
+   *
+   * @param {BatchUpdateDocuments} batch
+   */
+  uploadBatch: (batch: BatchUpdateDocuments) => Promise<AxiosResponse>;
+}
