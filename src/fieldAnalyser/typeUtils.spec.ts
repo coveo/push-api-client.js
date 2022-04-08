9e8b16bca4e09cc3cedaa6ebbd8fa5a794ed7a0a
y-lakhdar <ylakhdar@coveo.com>
2022-04-08 17:28:07 -0400

    fix: fix numeric type evolution

--- src/fieldAnalyser/typeUtils.spec.ts
+++ src/fieldAnalyser/typeUtils.spec.ts
@@ -1,5 +1,5 @@
 import {FieldTypes} from '@coveord/platform-client';
-import {getGuessedTypeFromValue, isValidTypeTransition} from './typeUtils';
+import {getGuessedTypeFromValue, getMostEnglobingType} from './typeUtils';
 
 describe('typeUtils', () => {
   const signedIntegerLimit = 0b1111111111111111111111111111111;
@@ -26,24 +26,58 @@ describe('typeUtils', () => {
 
   describe('when the transition is authorized', () => {
     it.each([
-      {from: FieldTypes.LONG, to: FieldTypes.LONG},
-      {from: FieldTypes.LONG, to: FieldTypes.LONG_64},
-      {from: FieldTypes.LONG, to: FieldTypes.DOUBLE},
-      {from: FieldTypes.LONG_64, to: FieldTypes.DOUBLE},
-      {from: FieldTypes.DOUBLE, to: FieldTypes.DOUBLE},
-    ])('should return true', ({from, to}) => {
-      expect(isValidTypeTransition(from, to)).toBe(true);
+      {
+        from: FieldTypes.LONG,
+        to: FieldTypes.LONG,
+        expectation: FieldTypes.LONG,
+      },
+      {
+        from: FieldTypes.LONG,
+        to: FieldTypes.LONG_64,
+        expectation: FieldTypes.LONG_64,
+      },
+      {
+        from: FieldTypes.LONG,
+        to: FieldTypes.DOUBLE,
+        expectation: FieldTypes.DOUBLE,
+      },
+      {
+        from: FieldTypes.LONG_64,
+        to: FieldTypes.DOUBLE,
+        expectation: FieldTypes.DOUBLE,
+      },
+      {
+        from: FieldTypes.DOUBLE,
+        to: FieldTypes.DOUBLE,
+        expectation: FieldTypes.DOUBLE,
+      },
+      {
+        from: FieldTypes.LONG_64,
+        to: FieldTypes.LONG,
+        expectation: FieldTypes.LONG_64,
+      },
+      {
+        from: FieldTypes.DOUBLE,
+        to: FieldTypes.LONG_64,
+        expectation: FieldTypes.DOUBLE,
+      },
+    ])('should return true', ({from, to, expectation}) => {
+      expect(getMostEnglobingType(from, to)).toBe(expectation);
     });
   });
 
   describe('when the transition is not authorized', () => {
     it.each([
-      {from: FieldTypes.LONG_64, to: FieldTypes.LONG},
-      {from: FieldTypes.DOUBLE, to: FieldTypes.LONG_64},
-      {from: FieldTypes.LONG_64, to: FieldTypes.STRING},
-      {from: FieldTypes.STRING, to: FieldTypes.LONG},
+      {
+        from: FieldTypes.LONG_64,
+        to: FieldTypes.STRING,
+      },
+      {
+        from: FieldTypes.STRING,
+        to: FieldTypes.LONG,
+      },
     ])('should return true', ({from, to}) => {
-      expect(isValidTypeTransition(from, to)).toBe(false);
+      expect(getMostEnglobingType(from, to)).toBe(null);
     });
   });
 });
