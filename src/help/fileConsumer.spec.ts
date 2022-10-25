3dc95c09d7105a2c0b536c6afb1d3531daed3bc9
ylakhdar <ylakhdar@coveo.com>
2022-10-24 22:29:24 -0400

    UT

--- src/help/fileConsumer.spec.ts
+++ src/help/fileConsumer.spec.ts
@@ -1,6 +1,8 @@
 jest.mock('axios');
 
+import {fileSync} from 'tmp';
 import {join} from 'path';
+import {writeFileSync} from 'fs';
 import {cwd} from 'process';
 import {
   FailedUploadCallback,
@@ -8,6 +10,30 @@ import {
   UploadBatchCallbackData,
 } from '..';
 import {FileConsumer} from './fileConsumer';
+
+/**
+ * Create a JSON file containing a document batch
+ *
+ * @param {number} count the number of items to add to the file (1 is the minimum)
+ * @param {number} size the body size (in byte) of each item
+ */
+
+function createBatch(count: number, size: number) {
+  const tmpDoc = fileSync({postfix: '.json'});
+  const template = (byte: number) => {
+    const randomCharacter = '#';
+    const data = Buffer.alloc(byte * 1024 * 1024, randomCharacter).toString();
+    return {
+      documentid: 'https://some.url.com',
+      title: 'Some Title',
+      data,
+    };
+  };
+
+  const content: string[] = Array(count).fill(template(size));
+  writeFileSync(tmpDoc.name, JSON.stringify(content));
+  return tmpDoc;
+}
 
 describe('FileConsumer', () => {
   let fileConsumer: FileConsumer;
@@ -21,10 +47,39 @@ describe('FileConsumer', () => {
   describe('when upload is successful', () => {
     beforeEach(() => {
       fileConsumer = new FileConsumer(fakeUpload, {maxConcurrent: 10});
-      fakeUpload.mockResolvedValueOnce({
+      fakeUpload.mockResolvedValue({
         status: 202,
         statusText: 'All good',
       });
+    });
+
+    afterEach(() => {
+      fakeUpload.mockReset();
+    });
+
+    it('should call callback on every batch upload', async () => {
+      const mockedHandleSuccess = jest.fn();
+      const documentCount = 5;
+      const documentSize = 2; // mb
+      const tmpDoc = createBatch(documentCount, documentSize);
+
+      fileConsumer.expectedDocumentCount = documentCount;
+      fileConsumer.onSuccess(mockedHandleSuccess);
+      await fileConsumer.consume([tmpDoc.name]);
+
+      expect(mockedHandleSuccess).toHaveBeenCalledTimes(3);
+      expect(mockedHandleSuccess).toHaveBeenNthCalledWith(
+        1,
+        expect.objectContaining({remainingDocumentCount: 3})
+      );
+      expect(mockedHandleSuccess).toHaveBeenNthCalledWith(
+        2,
+        expect.objectContaining({remainingDocumentCount: 1})
+      );
+      expect(mockedHandleSuccess).toHaveBeenNthCalledWith(
+        3,
+        expect.objectContaining({remainingDocumentCount: 0})
+      );
     });
 
     it('should call the success callback', async () => {
@@ -70,6 +125,26 @@ describe('FileConsumer', () => {
         status: 412,
         statusText: 'BAD_REQUEST',
       });
+    });
+
+    it('should return the number of remaining documents', async () => {
+      const mockedHandleError = jest.fn();
+      const documentCount = 5;
+      const documentSize = 2; // mb
+      const tmpDoc = createBatch(documentCount, documentSize);
+
+      fileConsumer.expectedDocumentCount = documentCount;
+      fileConsumer.onError(mockedHandleError);
+      await fileConsumer.consume([tmpDoc.name]);
+
+      expect(mockedHandleError).toHaveBeenCalledTimes(1);
+      expect(mockedHandleError).toHaveBeenCalledWith(
+        {
+          status: 412,
+          statusText: 'BAD_REQUEST',
+        },
+        expect.objectContaining({remainingDocumentCount: 3}) // 2 out of 5 document processed... 3 remaiming
+      );
     });
 
     it('should call the error callback', async () => {
