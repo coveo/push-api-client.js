47c645fcbfd2ba7867ad8019cc44e482593b08e5
ylakhdar <ylakhdar@coveo.com>
2022-11-03 17:15:58 -0400

    add UT for urlUtils

--- src/help/urlUtils.spec.ts
+++ src/help/urlUtils.spec.ts
@@ -1,12 +1,49 @@
-// TODO: should be simple enough
+import {Region} from '@coveord/platform-client';
+import {PlatformEnvironment} from '../environment';
+import {PushUrlBuilder, StreamUrlBuilder} from './urlUtils';
 
-// import {PushUrlBuilder} from './urlUtils';
+const platformOptions = {
+  region: Region.US,
+  environment: PlatformEnvironment.Prod,
+};
+describe('UrlBuilder', () => {
+  describe('PushUrlBuilder', () => {
+    let builder: PushUrlBuilder;
 
-// describe('UrlBuilder', () => {
-//   describe('PushUrlBuilder', () => {
-//     it('should return the base url', () => {
-//       new PushUrlBuilder('my-source-id');
-//     });
-//   });
-//   describe('StreamUrlBuilder', () => {});
-// });
+    beforeEach(() => {
+      builder = new PushUrlBuilder('my-source-id', 'org-id', platformOptions);
+    });
+
+    it('should return the base url', () => {
+      expect(builder.baseURL.toString()).toMatchSnapshot();
+    });
+
+    it('should return the base url for update', () => {
+      expect(builder.baseAPIURLForUpdate.toString()).toMatchSnapshot();
+    });
+
+    it('should return the file container url', () => {
+      expect(builder.fileContainerUrl.toString()).toMatchSnapshot();
+    });
+  });
+
+  describe('StreamUrlBuilder', () => {
+    let builder: StreamUrlBuilder;
+
+    beforeEach(() => {
+      builder = new StreamUrlBuilder('my-source-id', 'org-id', platformOptions);
+    });
+
+    it('should return the base url', () => {
+      expect(builder.baseStreamURL.toString()).toMatchSnapshot();
+    });
+
+    it('should return the base url for update', () => {
+      expect(builder.baseAPIURLForUpdate.toString()).toMatchSnapshot();
+    });
+
+    it('should return the file container url', () => {
+      expect(builder.fileContainerUrl.toString()).toMatchSnapshot();
+    });
+  });
+});
