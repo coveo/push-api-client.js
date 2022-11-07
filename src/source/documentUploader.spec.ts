df7847747f5e15807e7e4fc93bf151cba87cae8c
ylakhdar <ylakhdar@coveo.com>
2022-11-07 14:14:20 -0500

    add UT to documentUploader

--- src/source/documentUploader.spec.ts
+++ src/source/documentUploader.spec.ts
@@ -1,48 +1,167 @@
-// TODO: when calling with fileStrategy
-// TODO: when calling with stream strategy
+jest.mock('../fieldAnalyser/fieldAnalyser');
+jest.mock('../fieldAnalyser/fieldUtils');
+jest.mock('../APICore');
+jest.mock('./batchUploadDocumentsFromFile');
 
-// describe('when createFields option is set to true', () => {
-//   it.todo('should call Field Analyser');
-//   it.todo('should call #createFieldsFromReport');
-// });
+import PlatformClient from '@coveord/platform-client';
+import {DocumentBuilder} from '..';
+import {APICore} from '../APICore';
+import {FieldAnalyser} from '../fieldAnalyser/fieldAnalyser';
+import {createFieldsFromReport} from '../fieldAnalyser/fieldUtils';
+import {noop} from '../help/function';
+import {BatchUpdateDocuments, BatchUpdateDocumentsOptions} from '../interfaces';
+import {UploadStrategy} from '../uploadStrategy';
+import {BatchUploadDocumentsFromFilesReturn} from './batchUploadDocumentsFromFile';
+import {BuiltInTransformers} from '../validation/transformers/transformer';
+import {
+  uploadBatch,
+  uploadBatchFromFile,
+  uploadDocument,
+} from './documentUploader';
 
-// it('should create a file container', async () => {
-//   await uploadBatch('the_id', batch, {
-//     createFields: false,
-//   });
-//   expect(mockAxios.post).toHaveBeenCalledWith(
-//     'https://api.cloud.coveo.com/push/v1/organizations/the_org/files',
-//     expect.objectContaining({}),
-//     expectedDocumentsHeaders
-//   );
-// });
+const dummyClient = new PlatformClient({accessToken: 'my_token'});
+const dummyAPI = new APICore('my_token');
+const dummyStrategy: UploadStrategy = {
+  upload: jest.fn(),
+  preUpload: jest.fn(),
+  postUpload: jest.fn(),
+};
 
-// it('should upload files to container with returned upload uri and required headers ', async () => {
-//   await uploadBatch('the_id', batch, {
-//     createFields: false,
-//   });
-//   expect(mockAxios.put).toHaveBeenCalledWith(
-//     'https://fake.upload.url/',
-//     expect.objectContaining({
-//       addOrUpdate: expect.arrayContaining([
-//         expect.objectContaining({documentId: 'the_uri_1'}),
-//         expect.objectContaining({documentId: 'the_uri_2'}),
-//       ]),
-//       delete: expect.arrayContaining([
-//         expect.objectContaining({documentId: 'the_uri_3'}),
-//       ]),
-//     }),
-//     {headers: {foo: 'bar'}, maxBodyLength: 256e6}
-//   );
-// });
+describe('documentUploader', () => {
+  describe('#uploadDocument', () => {
+    const upload = (options?: BatchUpdateDocumentsOptions) =>
+      uploadDocument(
+        dummyClient,
+        new DocumentBuilder('http://some.url', 'Some document'),
+        new URL('https://fake.upload.url'),
+        dummyAPI,
+        options
+      );
 
-// it('should push content to source with returned fileId', async () => {
-//   await uploadBatch('the_id', batch, {
-//     createFields: false,
-//   });
-//   expect(mockAxios.put).toHaveBeenCalledWith(
-//     'https://api.cloud.coveo.com/push/v1/organizations/the_org/sources/the_id/documents/batch?fileId=file_id',
-//     {},
-//     expectedDocumentsHeaders
-//   );
-// });
+    it('should get field report from FieldAnalyser', async () => {
+      await upload({createFields: true});
+      expect(FieldAnalyser.prototype.report).toHaveBeenCalled();
+    });
+
+    it('should create missing fields from report', async () => {
+      await upload({createFields: true});
+      // TODO: go over all latest test and remove uncessary mocks !!!
+      expect(createFieldsFromReport).toHaveBeenCalled();
+    });
+
+    it('should not create missing fields', async () => {
+      await upload({createFields: false});
+      expect(createFieldsFromReport).not.toHaveBeenCalled();
+    });
+
+    it('should upload document', async () => {
+      await upload();
+      expect(APICore.prototype.put).toBeCalledWith(
+        'https://fake.upload.url/?documentId=http%3A%2F%2Fsome.url',
+        expect.objectContaining({
+          documentId: 'http://some.url',
+          title: 'Some document',
+        })
+      );
+    });
+  });
+
+  describe('#uploadBatch', () => {
+    const batch: BatchUpdateDocuments = {
+      addOrUpdate: [new DocumentBuilder('http://some.url', 'Some document')],
+      delete: [{documentId: 'foo_bar', deleteChildren: true}],
+    };
+    const upload = (options?: BatchUpdateDocumentsOptions) =>
+      uploadBatch(dummyClient, dummyStrategy, batch, options);
+
+    it('should get field report from FieldAnalyser', async () => {
+      await upload({createFields: true});
+      expect(FieldAnalyser.prototype.report).toHaveBeenCalled();
+    });
+
+    it('should create missing fields from report', async () => {
+      await upload({createFields: true});
+      // TODO: go over all latest test and remove uncessary mocks !!!
+      expect(createFieldsFromReport).toHaveBeenCalled();
+    });
+
+    it('should not create missing fields', async () => {
+      await upload({createFields: false});
+      expect(createFieldsFromReport).not.toHaveBeenCalled();
+    });
+
+    it('should execute the pre-upload strategy logic', async () => {
+      await upload();
+      expect(dummyStrategy.preUpload).toHaveBeenCalled();
+    });
+
+    it('should execute the upload strategy logic', async () => {
+      await upload();
+      expect(dummyStrategy.upload).toHaveBeenCalledWith({
+        addOrUpdate: [
+          expect.objectContaining({
+            uri: 'http://some.url',
+          }),
+        ],
+        delete: [
+          {
+            deleteChildren: true,
+            documentId: 'foo_bar',
+          },
+        ],
+      });
+    });
+
+    it('should execute the post-upload strategy logic', async () => {
+      await upload();
+      expect(dummyStrategy.postUpload).toHaveBeenCalled();
+    });
+  });
+
+  describe('#uploadBatchFromFile', () => {
+    const dummyCallback = jest.fn();
+
+    it('should call BatchUploadDocumentsFromFilesReturn with default options', async () => {
+      await uploadBatchFromFile(dummyClient, dummyStrategy, [
+        'path/to/file',
+        'other/path',
+      ]);
+      expect(BatchUploadDocumentsFromFilesReturn).toHaveBeenCalledWith(
+        dummyClient,
+        dummyStrategy,
+        ['path/to/file', 'other/path'],
+        {
+          callback: noop,
+          createFields: true,
+          fieldNameTransformer: BuiltInTransformers.identity,
+          maxConcurrent: 10,
+        }
+      );
+    });
+
+    it('should call BatchUploadDocumentsFromFilesReturn with custom options', async () => {
+      await uploadBatchFromFile(
+        dummyClient,
+        dummyStrategy,
+        ['path/to/file', 'other/path'],
+        {
+          createFields: false,
+          maxConcurrent: 3,
+          fieldNameTransformer: BuiltInTransformers.toLowerCase,
+          callback: dummyCallback,
+        }
+      );
+      expect(BatchUploadDocumentsFromFilesReturn).toHaveBeenCalledWith(
+        dummyClient,
+        dummyStrategy,
+        ['path/to/file', 'other/path'],
+        {
+          callback: dummyCallback,
+          createFields: false,
+          fieldNameTransformer: BuiltInTransformers.toLowerCase,
+          maxConcurrent: 3,
+        }
+      );
+    });
+  });
+});
