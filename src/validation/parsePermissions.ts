97a1a19af6453f86a9d31eff1b8db5ab514399c5
ylakhdar <ylakhdar@coveo.com>
2022-12-16 14:39:29 -0500

    parse permissions

---
+++ src/validation/parsePermissions.ts
@@ -0,0 +1,225 @@
+import {PathLike} from 'fs';
+import {
+  ArrayValue,
+  BooleanValue,
+  PrimitivesValues,
+  RecordValue,
+  StringValue,
+} from '@coveo/bueno';
+import {Document} from '../document';
+import {AnySecurityIdentityBuilder} from '../securityIdentityBuilder';
+import {CaseInsensitiveDocument, Recordable} from './caseInsensitiveDocument';
+import {DocumentBuilder} from '../documentBuilder';
+import {InvalidDocument} from '../errors';
+import {KnownKeys} from './knownKey';
+import {PermissionSection, PermissionSetBuilder} from '../permissionSetBuilder';
+import {RequiredKeyValidator} from './requiredKeyValidator';
+import {
+  PermissionSet,
+  PermissionLevel,
+  Permission,
+} from '@coveo/platform-client';
+
+export const processPermissionList = (
+  caseInsensitiveDoc: CaseInsensitiveDocument<PrimitivesValues>,
+  documentBuilder: DocumentBuilder,
+  documentPath: PathLike
+) => {
+  ensurePermissionArray(caseInsensitiveDoc, documentPath);
+  new KnownKeys<Document['permissions']>(
+    'permissions',
+    caseInsensitiveDoc
+  ).whenExists((permissions) => {
+    permissions!.forEach(
+      (permission: Recordable<PermissionSet> | Recordable<PermissionLevel>) => {
+        const caseInsensitivePermission =
+          new CaseInsensitiveDocument<PrimitivesValues>(permission);
+
+        new KnownKeys('permissionsets', caseInsensitivePermission)
+          .whenExists<PermissionLevel>((permissionLevel) =>
+            processPermissionLevel(
+              permissionLevel,
+              documentBuilder,
+              documentPath
+            )
+          )
+          .whenDoesNotExist<PermissionSet>((permissionSet) =>
+            processPermissionSet(permissionSet, documentBuilder, documentPath)
+          );
+      }
+    );
+
+    delete caseInsensitiveDoc.documentRecord['permissions'];
+  });
+};
+
+const processPermissionSet = (
+  permissionSet: PermissionSet,
+  documentBuilder: DocumentBuilder,
+  documentPath: PathLike
+) => {
+  const permissionSetBuilder =
+    validateRequiredPermissionSetKeysAndGetPermissionSetBuilder(
+      permissionSet,
+      documentPath
+    );
+
+  documentBuilder.withPermissionSet(permissionSetBuilder);
+};
+
+const processPermissionLevel = (
+  permission: Recordable<PermissionLevel>,
+  documentBuilder: DocumentBuilder,
+  documentPath: PathLike
+) => {
+  // TODO: require at least some permissions
+  const permissionSetBuilders = permission.permissionSets!.map(
+    (permissionSet) => {
+      const caseInsensitivePermissions = new CaseInsensitiveDocument(
+        permission
+      );
+      const requiredPermissionLevelName = new RequiredKeyValidator<string>(
+        'name',
+        caseInsensitivePermissions,
+        new StringValue({required: true, emptyAllowed: false})
+      );
+      if (!requiredPermissionLevelName.isValid) {
+        throw new InvalidDocument(
+          documentPath,
+          requiredPermissionLevelName.explanation
+        );
+      }
+      return validateRequiredPermissionSetKeysAndGetPermissionSetBuilder(
+        permissionSet,
+        documentPath
+      );
+    }
+  );
+
+  // TODO: prevent empty permission set
+  documentBuilder.withPermissionLevel(permission.name!, permissionSetBuilders);
+};
+
+const validateAllowAnonymous = (
+  caseInsensitivePermissions: CaseInsensitiveDocument<PrimitivesValues>,
+  documentPath: PathLike
+): boolean | never => {
+  const requiredAllowAnonymous = new RequiredKeyValidator(
+    'allowanonymous',
+    caseInsensitivePermissions,
+    new BooleanValue({required: true})
+  );
+  if (!requiredAllowAnonymous.isValid) {
+    throw new InvalidDocument(documentPath, requiredAllowAnonymous.explanation);
+  }
+  return requiredAllowAnonymous.value!;
+};
+
+const validatePermission = (
+  permissionLogic: Lowercase<PermissionSection>,
+  caseInsensitivePermissions: CaseInsensitiveDocument<PrimitivesValues>,
+  documentPath: PathLike
+) => {
+  const requiredPermission = new RequiredKeyValidator(
+    permissionLogic,
+    caseInsensitivePermissions,
+    getSecurityIdentitySchemaValidation()
+  );
+
+  if (!requiredPermission.isValid) {
+    throw new InvalidDocument(documentPath, requiredPermission.explanation);
+  }
+
+  return requiredPermission.value;
+};
+
+const validateRequiredPermissionSetKeysAndGetPermissionSetBuilder = (
+  permission: Recordable<PermissionSet>,
+  documentPath: PathLike
+): PermissionSetBuilder => {
+  const caseInsensitivePermissions = new CaseInsensitiveDocument(permission);
+  const allowAnonymous = validateAllowAnonymous(
+    caseInsensitivePermissions,
+    documentPath
+  );
+  const allowedPermissions = validatePermission(
+    'allowedpermissions',
+    caseInsensitivePermissions,
+    documentPath
+  );
+  const deniedPermissions = validatePermission(
+    'deniedpermissions',
+    caseInsensitivePermissions,
+    documentPath
+  );
+
+  const permissionSetBuilder = new PermissionSetBuilder(allowAnonymous);
+
+  allowedPermissions?.forEach(
+    ({identity, identityType, securityProvider}: Permission) => {
+      permissionSetBuilder.withAllowedPermissions(
+        new AnySecurityIdentityBuilder(
+          identityType!,
+          identity!,
+          securityProvider
+        )
+      );
+    }
+  );
+
+  deniedPermissions?.forEach(
+    ({identity, identityType, securityProvider}: Permission) => {
+      permissionSetBuilder.withDeniedPermissions(
+        new AnySecurityIdentityBuilder(
+          identityType!,
+          identity!,
+          securityProvider
+        )
+      );
+    }
+  );
+
+  return permissionSetBuilder;
+};
+
+const getSecurityIdentitySchemaValidation = (): ArrayValue<Permission> => {
+  return new ArrayValue({
+    required: true,
+    each: new RecordValue({
+      values: {
+        identity: new StringValue({required: true, emptyAllowed: false}),
+        identityType: new StringValue({
+          constrainTo: ['UNKNOWN', 'USER', 'GROUP', 'VIRTUAL_GROUP'],
+          required: true,
+          emptyAllowed: false,
+        }),
+        securityProvider: new StringValue({
+          emptyAllowed: false,
+          required: true,
+        }),
+      },
+    }),
+  });
+};
+
+const ensurePermissionArray = (
+  caseInsensitiveDoc: CaseInsensitiveDocument<PrimitivesValues>,
+  documentPath: PathLike
+) => {
+  const validator = (schema: ArrayValue<PrimitivesValues>) =>
+    new RequiredKeyValidator('permissions', caseInsensitiveDoc, schema);
+  const requiredPermissionArray = validator(new ArrayValue({required: false}));
+  const requiredAtLeastOnePermission = validator(new ArrayValue({min: 1}));
+
+  if (!requiredPermissionArray.isValid) {
+    throw new InvalidDocument(
+      documentPath,
+      requiredPermissionArray.explanation
+    );
+  }
+
+  if (!requiredAtLeastOnePermission.isValid) {
+    // simply discard empty array permission instead of throwing
+    delete caseInsensitiveDoc.documentRecord['permissions'];
+  }
+};
